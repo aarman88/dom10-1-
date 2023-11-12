b4928d7edf2195247cadddd8cd1f4ae5824cad01
Арман <@ARMAN>
2023-11-12 15:45:22 +0600

    Добавьте файлы проекта.

---
+++ dom10(1)/ICalculatable.cs
@@ -0,0 +1,7 @@
+﻿public interface ICalculatable
+{
+    double Add(double a, double b);
+    double Subtract(double a, double b);
+    double Multiply(double a, double b);
+    double Divide(double a, double b);
+}
---
+++ dom10(1)/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Calculator : ICalculatable
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException("Деление на ноль недопустимо.");
+        }
+        return a / b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ICalculatable calculator = new Calculator();
+
+        double result = calculator.Add(5, 3);
+        Console.WriteLine("5 + 3 = " + result);
+
+        result = calculator.Subtract(10, 4);
+        Console.WriteLine("10 - 4 = " + result);
+
+        result = calculator.Multiply(7, 2);
+        Console.WriteLine("7 * 2 = " + result);
+
+        result = calculator.Divide(9, 3);
+        Console.WriteLine("9 / 3 = " + result);
+    }
+}
